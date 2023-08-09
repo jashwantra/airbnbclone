b59a63047e60ed587b98dcbcf2264af9e2b6c9ed
Jashwant <jashwantra@gmail.com>
2023-08-10 02:09:17 +0530

    Before firebase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import PlacesFormPage from './pages/PlacesFormPage';
 import PlacePage from './pages/PlacePage';
 import BookingsPage from './pages/BookingsPage';
 
-axios.defaults.baseURL = "https://wild-teal-butterfly-garb.cyclic.app";
+axios.defaults.baseURL = "https://airbnbcloneapi.onrender.com";
 axios.defaults.withCredentials = true;
 
 function App() {
